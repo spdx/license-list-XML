595cdc8a3673a46e7cbbe193effb10b7c61df0f9
Gary O'Neall <gary@sourceauditor.com>
2018-01-02 14:34:46 -0800

    Address review comments for pull request
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- .travis/testBuildLicenseList.sh
+++ .travis/testBuildLicenseList.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 TOOLSJAR=spdx-tools-2.1.8-jar-with-dependencies.jar
-LICENSEXMLDIR=./
-TESTDIR=./test/original
-LICENSEOUTPUTDIR=.OUTPUTFILES
+LICENSE_XML_DIR=./
+TEST_DIR=./test/original
+LICENSE_OUTPUT_DIR=.OUTPUTFILES
 VERSION=$(git describe)
 if [[ $? != 0 ]]; then VERSION="UNKNOWN"; fi
-RELEASEDATE=$(date -I)
-mkdir $LICENSEOUTPUTDIR
+RELEASE_DATE=$(date -I)
+mkdir $LICENSE_OUTPUT_DIR
 # Create a file to skip expected warnings
-IGNOREWARNINGS="\"Duplicates licenses: AGPL-3.0-or-later, AGPL-3.0-only\",\"Duplicates licenses: GFDL-1.1-or-later, GFDL-1.1-only\",\"Duplicates licenses: GFDL-1.2-or-later, GFDL-1.2-only\",\"Duplicates licenses: GFDL-1.3-or-later, GFDL-1.3-only\",\"Duplicates licenses: GPL-1.0-or-later, GPL-1.0-only\",\"Duplicates licenses: GPL-2.0-or-later, GPL-2.0-only\",\"Duplicates licenses: GPL-3.0-or-later, GPL-3.0-only\",\"Duplicates licenses: LGPL-2.0-or-later, LGPL-2.0-only\",\"Duplicates licenses: LGPL-2.1-or-later, LGPL-2.1-only\",\"Duplicates licenses: LGPL-3.0-or-later, LGPL-3.0-only\",\"Duplicates licenses: MPL-2.0, MPL-2.0-no-copyleft-exception\""
-WARNINGSFILE="TEMP-WARNINGS"
-echo $IGNOREWARNINGS > $WARNINGSFILE
+IGNORE_WARNINGS="\"Duplicates licenses: AGPL-3.0-or-later, AGPL-3.0-only\",\"Duplicates licenses: GFDL-1.1-or-later, GFDL-1.1-only\",\"Duplicates licenses: GFDL-1.2-or-later, GFDL-1.2-only\",\"Duplicates licenses: GFDL-1.3-or-later, GFDL-1.3-only\",\"Duplicates licenses: GPL-1.0-or-later, GPL-1.0-only\",\"Duplicates licenses: GPL-2.0-or-later, GPL-2.0-only\",\"Duplicates licenses: GPL-3.0-or-later, GPL-3.0-only\",\"Duplicates licenses: LGPL-2.0-or-later, LGPL-2.0-only\",\"Duplicates licenses: LGPL-2.1-or-later, LGPL-2.1-only\",\"Duplicates licenses: LGPL-3.0-or-later, LGPL-3.0-only\",\"Duplicates licenses: MPL-2.0, MPL-2.0-no-copyleft-exception\""
+WARNINGS_FILE="TEMP-WARNINGS"
+echo $IGNORE_WARNINGS > $WARNINGS_FILE
 # Test and generate the output files
-PUBLISHCMD="LicenseRDFAGenerator $LICENSEXMLDIR $LICENSEOUTPUTDIR $VERSION $RELEASEDATE $TESTDIR $WARNINGSFILE"
-echo running SPDX Tool command $PUBLISHCMD
-java -jar $TOOLSJAR $PUBLISHCMD
+PUBLISH_CMD="LicenseRDFAGenerator $LICENSE_XML_DIR $LICENSE_OUTPUT_DIR $VERSION $RELEASE_DATE $TEST_DIR $WARNINGS_FILE"
+echo running SPDX Tool command $PUBLISH_CMD
+java -jar $TOOLSJAR $PUBLISH_CMD
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-rm $WARNINGSFILE
+rm $WARNINGS_FILE
 echo License list has been validated.
